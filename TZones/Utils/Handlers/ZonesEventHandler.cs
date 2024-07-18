e9f4cfa67e10940c40f7c99ace4be0573a721227
TavstalDev <tavstal@gmail.com>
2024-07-18 16:45:11 +0200

    Added event bases

--- TZones/Utils/Handlers/ZonesEventHandler.cs
+++ TZones/Utils/Handlers/ZonesEventHandler.cs
@@ -1,12 +1,52 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Rocket.Unturned.Player;
+using Tavstal.TZones.Models.Core;
+using Tavstal.TZones.Utils.Managers;
 
 namespace Tavstal.TZones.Utils.Handlers
 {
-    public class ZonesEventHandler
+    public static class ZonesEventHandler
     {
-        
+        private static bool _isAttached = false;
+
+
+        public static void AttachEvents()
+        {
+            if (_isAttached)
+                return;
+
+            _isAttached = true;
+
+            ZonesManager.OnPlayerEnterZone += OnPlayerEnterZone;
+            ZonesManager.OnPlayerLeaveZone += OnPlayerLeaveZone;
+            ZonesManager.OnZoneUpdated += OnZoneUpdated;
+        }
+
+        public static void DetachEvents()
+        {
+            if (!_isAttached)
+                return;
+
+            _isAttached = false;
+
+            ZonesManager.OnPlayerEnterZone -= OnPlayerEnterZone;
+            ZonesManager.OnPlayerLeaveZone -= OnPlayerLeaveZone;
+            ZonesManager.OnZoneUpdated -= OnZoneUpdated;
+        }
+
+        private static void OnPlayerEnterZone(UnturnedPlayer player, Zone zone)
+        {
+            
+        }
+
+        private static void OnPlayerLeaveZone(UnturnedPlayer player, Zone zone)
+        {
+            
+        }
+
+        private static void OnZoneUpdated(Zone zone)
+        {
+            
+        }
     }
 }
--- TZones/Utils/Managers/ZonesManager.cs
+++ TZones/Utils/Managers/ZonesManager.cs
@@ -1,9 +1,10 @@
 using System.Collections.Generic;
+using Rocket.Unturned.Player;
 using Tavstal.TZones.Models.Core;
 
 namespace Tavstal.TZones.Utils.Managers
 {
-    public class ZonesManager
+    public static class ZonesManager
     {
         // Because the unturned events use 'ref', and the database is async, cache must be used
         private static List<Flag> _flags { get; set; }
@@ -13,6 +14,30 @@ namespace Tavstal.TZones.Utils.Managers
         private static Dictionary<ulong, List<ZoneEvent>> _zoneEvents { get; set; }
         private static Dictionary<ulong, List<Block>> _zoneBlocks { get; set; }
 
+        public delegate void PlayerEnterZonedHandler(UnturnedPlayer player, Zone zone);
+        public static event PlayerEnterZonedHandler OnPlayerEnterZone;
+        internal static void FPlayerEnterZone(UnturnedPlayer player, Zone zone)
+        {
+            OnPlayerEnterZone?.Invoke(player, zone);
+        }
+
+        public delegate void PlayerLeaveZonedHandler(UnturnedPlayer player, Zone zone);
+        public static event PlayerLeaveZonedHandler OnPlayerLeaveZone;
+        internal static void FPlayerLeaveZone(UnturnedPlayer player, Zone zone)
+        {
+            OnPlayerLeaveZone?.Invoke(player, zone);
+        }
+
+        public delegate void ZoneUpdatedHandler(Zone zone);
+        public static event ZoneUpdatedHandler OnZoneUpdated;
+        internal static void FZoneUpdated(Zone zone)
+        {
+            OnZoneUpdated?.Invoke(zone);
+        }
+
+        public static void Refresh() {
+
+        }
         
     }
 }
