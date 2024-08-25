f3f50acf3f96a86161e2c0fe93691da00c903a8d
TavstalDev <tavstal@gmail.com>
2024-08-25 20:00:14 +0200

    Minor changes and fixes

--- TZones/Utils/Managers/ZonesManager.cs
+++ TZones/Utils/Managers/ZonesManager.cs
@@ -5,12 +5,19 @@ using Rocket.Unturned.Player;
 using Tavstal.TZones.Models.Core;
 using Tavstal.TZones.Models.Enums;
 using UnityEngine;
+using Tavstal.TZones.Components;
+using SDG.Unturned;
+using Block = Tavstal.TZones.Models.Core.Block;
+using ENodeType = Tavstal.TZones.Models.Enums.ENodeType;
+using Flag = Tavstal.TZones.Models.Core.Flag;
+using Node = Tavstal.TZones.Models.Core.Node;
 
 namespace Tavstal.TZones.Utils.Managers
 {
     public static class ZonesManager
     {
         #region Fields
+
         private static bool _isDirty { get; set; }
         // Because the unturned events use 'ref', and the database is async, cache must be used
         private static List<Flag> _flags { get; set; }
@@ -29,18 +36,18 @@ namespace Tavstal.TZones.Utils.Managers
         #endregion
         
         #region Events
-        public delegate void PlayerEnterZonedHandler(UnturnedPlayer player, Zone zone, Vector3 lastPosition);
+        public delegate void PlayerEnterZonedHandler(UnturnedPlayer player, Zone zone, Vector3 lastPosition, ref bool shouldAllow);
         public static event PlayerEnterZonedHandler OnPlayerEnterZone;
-        internal static void FPlayerEnterZone(UnturnedPlayer player, Zone zone, Vector3 lastPosition)
+        internal static void FPlayerEnterZone(UnturnedPlayer player, Zone zone, Vector3 lastPosition,  ref bool shouldAllow)
         {
-            OnPlayerEnterZone?.Invoke(player, zone, lastPosition);
+            OnPlayerEnterZone?.Invoke(player, zone, lastPosition, ref shouldAllow);
         }
 
-        public delegate void PlayerLeaveZonedHandler(UnturnedPlayer player, Zone zone, Vector3 lastPosition);
+        public delegate void PlayerLeaveZonedHandler(UnturnedPlayer player, Zone zone, Vector3 lastPosition, ref bool shouldAllow);
         public static event PlayerLeaveZonedHandler OnPlayerLeaveZone;
-        internal static void FPlayerLeaveZone(UnturnedPlayer player, Zone zone, Vector3 lastPosition)
+        internal static void FPlayerLeaveZone(UnturnedPlayer player, Zone zone, Vector3 lastPosition, ref bool shouldAllow)
         {
-            OnPlayerLeaveZone?.Invoke(player, zone, lastPosition);
+            OnPlayerLeaveZone?.Invoke(player, zone, lastPosition, ref shouldAllow);
         }
 
         public delegate void ZoneCreatedHandler(Zone zone);
@@ -83,30 +90,30 @@ namespace Tavstal.TZones.Utils.Managers
 
         private static async Task RefreshAllAsync() 
         {
-            _flags = await TZones.DatabaseManager.GetFlagsAsync(string.Empty);
-            _zones = await TZones.DatabaseManager.GetZonesAsync(string.Empty);
+            _flags = await TZones.DatabaseManager.GetFlagsAsync(string.Empty) ?? new List<Flag>();
+            _zones = await TZones.DatabaseManager.GetZonesAsync(string.Empty) ?? new List<Zone>();
             _nodes = new Dictionary<ulong, List<Node>>();
             _zoneFlags = new Dictionary<ulong, List<ZoneFlag>>();
             _zoneEvents = new Dictionary<ulong, List<ZoneEvent>>();
             _zoneBlocks = new Dictionary<ulong, List<Block>>();
             foreach (Zone zone in _zones) {
                 #region Nodes
-                List<Node> nodes = (await TZones.DatabaseManager.GetNodesAsync($"ZoneId=`{zone.Id}`")) ?? new List<Node>();
+                List<Node> nodes = (await TZones.DatabaseManager.GetNodesAsync($"ZoneId='{zone.Id}'")) ?? new List<Node>();
                 _nodes.Add(zone.Id, nodes);
                 #endregion
 
                 #region Flags
-                List<ZoneFlag> flags = (await TZones.DatabaseManager.GetZoneFlagsAsync($"ZoneId=`{zone.Id}`")) ?? new List<ZoneFlag>();
+                List<ZoneFlag> flags = (await TZones.DatabaseManager.GetZoneFlagsAsync($"ZoneId='{zone.Id}'")) ?? new List<ZoneFlag>();
                 _zoneFlags.Add(zone.Id, flags);
                 #endregion
 
                 #region Events
-                List<ZoneEvent> events = (await TZones.DatabaseManager.GetZoneEventsAsync($"ZoneId=`{zone.Id}`")) ?? new List<ZoneEvent>();
+                List<ZoneEvent> events = (await TZones.DatabaseManager.GetZoneEventsAsync($"ZoneId='{zone.Id}'")) ?? new List<ZoneEvent>();
                 _zoneEvents.Add(zone.Id, events);
                 #endregion
 
                 #region Blocks
-                List<Block> blocks = (await TZones.DatabaseManager.GetBlocksAsync($"ZoneId=`{zone.Id}`")) ?? new List<Block>();
+                List<Block> blocks = (await TZones.DatabaseManager.GetBlocksAsync($"ZoneId='{zone.Id}'")) ?? new List<Block>();
                 _zoneBlocks.Add(zone.Id, blocks);
                 #endregion
             }
@@ -122,27 +129,27 @@ namespace Tavstal.TZones.Utils.Managers
                 _zoneEvents.Remove(zone.Id);
                 _zoneBlocks.Remove(zone.Id);
             }
-            zone = await TZones.DatabaseManager.FindZoneAsync($"Id=`{zoneId}`");
+            zone = await TZones.DatabaseManager.FindZoneAsync($"Id='{zoneId}'");
             if (zone != null) {
                 _zones.Add(zone);
 
                 #region Nodes
-                List<Node> nodes = (await TZones.DatabaseManager.GetNodesAsync($"ZoneId=`{zone.Id}`")) ?? new List<Node>();
+                List<Node> nodes = (await TZones.DatabaseManager.GetNodesAsync($"ZoneId='{zone.Id}'")) ?? new List<Node>();
                 _nodes.Add(zone.Id, nodes);
                 #endregion
 
                 #region Flags
-                List<ZoneFlag> flags = (await TZones.DatabaseManager.GetZoneFlagsAsync($"ZoneId=`{zone.Id}`")) ?? new List<ZoneFlag>();
+                List<ZoneFlag> flags = (await TZones.DatabaseManager.GetZoneFlagsAsync($"ZoneId='{zone.Id}'")) ?? new List<ZoneFlag>();
                 _zoneFlags.Add(zone.Id, flags);
                 #endregion
 
                 #region Events
-                List<ZoneEvent> events = (await TZones.DatabaseManager.GetZoneEventsAsync($"ZoneId=`{zone.Id}`")) ?? new List<ZoneEvent>();
+                List<ZoneEvent> events = (await TZones.DatabaseManager.GetZoneEventsAsync($"ZoneId='{zone.Id}'")) ?? new List<ZoneEvent>();
                 _zoneEvents.Add(zone.Id, events);
                 #endregion
 
                 #region Blocks
-                List<Block> blocks = (await TZones.DatabaseManager.GetBlocksAsync($"ZoneId=`{zone.Id}`")) ?? new List<Block>();
+                List<Block> blocks = (await TZones.DatabaseManager.GetBlocksAsync($"ZoneId='{zone.Id}'")) ?? new List<Block>();
                 _zoneBlocks.Add(zone.Id, blocks);
                 #endregion
             }
@@ -161,7 +168,7 @@ namespace Tavstal.TZones.Utils.Managers
 
         public static async Task<int> RemoveCustomFlagAsync(string name)
         {
-            Flag targetFlag = ZonesManager.Flags.Find(x => x.Name == name);
+            Flag targetFlag = Flags.Find(x => x.Name == name);
             if (targetFlag == null)
                 return 1;
 
@@ -294,6 +301,105 @@ namespace Tavstal.TZones.Utils.Managers
                 return blocks.Any(x => x.UnturnedId == unturnedId && x.Type == blockType);
             }
             return false;
+        }
+        #endregion
+
+        #region  Unity Update
+        internal static async Task UpdateUnityAsync()
+        {
+            await CheckDirtyAsync();
+            
+            // Note: 
+            // Future performance issues might be solved with Parallel.ForEach instead of regular foreach
+            // Only use it at heavy load, or else it won't make it faster
+            UpdatePlayers();
+
+            // Update Generators & Zombies
+            if (Zones == null)
+                return;
+            
+            Parallel.ForEach(Zones, zone => {
+                UpdateGenerators(zone);
+                UpdateZombies(zone);
+            });
+        }
+        
+        private static void UpdatePlayers() 
+        {
+            foreach (SteamPlayer steamPlayer in Provider.clients) 
+            {
+                UnturnedPlayer uPlayer = UnturnedPlayer.FromSteamPlayer(steamPlayer);
+                ZonePlayerComponent comp = uPlayer.GetComponent<ZonePlayerComponent>();
+
+                List<Zone> currentZones = GetZonesFromPosition(uPlayer.Position);
+                bool updateLastPos = true;
+
+                foreach (Zone zone in comp.Zones) 
+                {
+                    if (currentZones.All(x => x.Id != zone.Id))
+                    {
+                        bool shouldAllow = true;
+                        OnPlayerLeaveZone?.Invoke(uPlayer, zone, comp.LastPosition, ref shouldAllow);
+                        if (!shouldAllow)
+                        {
+                            currentZones.Add(zone);
+                            updateLastPos = false;
+                        }
+                    }
+                }
+
+                foreach (Zone zone in currentZones.ToList()) {
+                    if (comp.Zones.All(x => x.Id != zone.Id)) 
+                    {
+                        bool shouldAllow = true;
+                        OnPlayerEnterZone?.Invoke(uPlayer, zone, comp.LastPosition, ref shouldAllow);
+                        if (!shouldAllow)
+                        {
+                            currentZones.Remove(zone);
+                            updateLastPos = false;
+                        }
+                    }
+                }
+
+                comp.Zones = currentZones;
+                if (updateLastPos)
+                    comp.LastPosition = uPlayer.Position;
+            }
+        }
+
+        private static void UpdateZombies(Zone zone) 
+        {
+            
+            if (zone.HasFlag(Constants.Flags.NoZombie) && ZombieManager.regions != null) 
+            {
+                foreach (ZombieRegion t in ZombieManager.regions.Where(t => t.zombies != null))
+                {
+                    foreach (var zombie in t.zombies.Where(z => z))
+                    {
+                        if (zombie.isDead) 
+                            continue;
+                        if (!zone.IsPointInZone(zombie.transform.position)) 
+                            continue;
+                        zombie.gear = 0;
+                        zombie.isDead = true;
+                        ZombieManager.sendZombieDead(zombie, Vector3.zero);
+                    }
+                }
+            }
+        }
+
+        // ReSharper disable Unity.PerformanceAnalysis
+        private static void UpdateGenerators(Zone zone)
+        {
+            if (!zone.HasFlag(Constants.Flags.InfiniteGenerator))
+                return;
+            InteractableGenerator[] generators = Object.FindObjectsOfType<InteractableGenerator>();
+            foreach (var generator in generators)
+            {
+                if (zone.IsPointInZone(generator.transform.position) &&
+                    generator.fuel < generator.capacity - 10)
+                    BarricadeManager.sendFuel(generator.transform, generator.capacity);
+            }
         }
         #endregion
     }
