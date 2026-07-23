95896468638d51dc33ec27a8f1dfe1bb20733cd9
tavstal <tavstal@gmail.com>
2026-07-23 23:17:36 +0200

    refactor: update DatabaseData class to inherit from DatabaseConfigBase and use Yaml attributes

--- TZones/Models/Database/DatabaseData.cs
+++ TZones/Models/Database/DatabaseData.cs
@@ -1,32 +1,12 @@
-﻿using Newtonsoft.Json;
-using Tavstal.TLibrary.Models.Database;
+﻿using Tavstal.TLibrary.Models.Config;
+using YamlDotNet.Serialization;
 
 namespace Tavstal.TZones.Models.Database
 {
-    public class DatabaseData : DatabaseSettingsBase
+    public class DatabaseData : DatabaseConfigBase
     {
         // Note: It starts from 7 because there are 6 defined property in the base class
-        [JsonProperty(Order = 7)]
-        public string TableZones { get; set; }
-        [JsonProperty(Order = 8)]
-        public string TableZoneNodes { get; set; }
-        [JsonProperty(Order = 9)]
-        public string TableFlags { get; set; }
-        [JsonProperty(Order = 10)]
-        public string TableZoneFlags { get; set; }
-        [JsonProperty(Order = 11)]
-        public string TableZoneEvents { get; set; }
-        [JsonProperty(Order = 12)]
-        public string TableZoneBlocklist { get; set; }
-
-        public DatabaseData(string tableZones, string tableZoneNodes, string tableFlags, string tableZoneFlags, string tableZoneEvents, string tableZoneBlocklist)
-        {
-            TableZones = tableZones;
-            TableZoneNodes = tableZoneNodes;
-            TableFlags = tableFlags;
-            TableZoneFlags = tableZoneFlags;
-            TableZoneEvents = tableZoneEvents;
-            TableZoneBlocklist = tableZoneBlocklist;
-        }
+        [YamlMember(Order = 7)] 
+        public string TablePrefix { get; set; } = "tzones_";
     }
 }
