9d54b287bc2cc4dd87d4be1c4f5e080faf50e08d
tavstal <tavstal@gmail.com>
2026-07-23 23:17:12 +0200

    refactor: initialize Zones property with a new HashSet instance

--- TZones/Components/ZonePlayerComponent.cs
+++ TZones/Components/ZonePlayerComponent.cs
@@ -1,14 +1,13 @@
 using System;
 using System.Collections.Generic;
 using Rocket.Unturned.Player;
-using Tavstal.TZones.Models.Core;
 using UnityEngine;
 
 namespace Tavstal.TZones.Components
 {
     public class ZonePlayerComponent : UnturnedPlayerComponent
     {
-        public HashSet<ulong> Zones { get; set; }
+        public HashSet<ulong> Zones { get; set; } = new  HashSet<ulong>();
         public Vector3 LastPosition { get; set; }
         public DateTime SpamPreventEnd { get; set; }
 
