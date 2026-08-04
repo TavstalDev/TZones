aeb173401986dd51f59b1b36b2b8b81c697e96f3
tavstal <tavstal@proton.me>
2026-08-04 14:42:56 +0200

    fix: correct spelling of EQUP to EQUIP and rename VEHICLE_LEAVE to VEHICLE_EXIT

--- TZones/Models/Enums/ERestrictionType.cs
+++ TZones/Models/Enums/ERestrictionType.cs
@@ -8,12 +8,12 @@ namespace Tavstal.TZones.Models.Enums
         /// <summary>Blocks building/placing the item.</summary>
         BUILD = 0,
         /// <summary>Blocks equipping the item.</summary>
-        EQUP = 1,
+        EQUIP = 1,
         /// <summary>Blocks unequipping the item.</summary>
         UNEQUIP = 2,
         /// <summary>Blocks entering the vehicle.</summary>
         VEHICLE_ENTER = 3,
         /// <summary>Blocks exiting the vehicle.</summary>
-        VEHICLE_LEAVE = 4,
+        VEHICLE_EXIT = 4,
     }
 }
